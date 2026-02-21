81d13f940989a3f40df8e2aa086dee2b6855c6cc
Dhaniya1 <ndohare11@gmail.com>
2026-02-21 22:14:37 +0530

    updated gitignore file

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,14 +1,16 @@
 import axios from "axios";
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 export const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: apiUrl,
 });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
-  if(token) {
-    config.headers.Authorization = `Bearer ${token}`
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
