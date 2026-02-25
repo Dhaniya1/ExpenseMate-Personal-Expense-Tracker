dcee05ac2959e354ef7a3f36358e2817b6a615db
Dhaniya1 <ndohare11@gmail.com>
2026-02-25 22:02:00 +0530

    bug fixes

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["expensemate-gold.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
