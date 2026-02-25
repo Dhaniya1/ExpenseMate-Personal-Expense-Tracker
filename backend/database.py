07fb39aa0ae7601a10ab4eaca0f92520eb4258c4
Dhaniya1 <ndohare11@gmail.com>
2026-02-25 23:48:49 +0530

    bug fixes

--- backend/database.py
+++ backend/database.py
@@ -1,19 +1,17 @@
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from sqlalchemy import create_engine
-from sqlalchemy.pool import NullPool
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, dotenv_values
 
 load_dotenv()
 server_password = os.getenv("PASSWORD")
-DATABASE_URL = f"postgresql://postgres.igozekdqvmbjbauputsu:{server_password}@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres?sslmode=require"
+DATABASE_URL = f"postgresql://postgres.igozekdqvmbjbauputsu:{server_password}@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres"
+
 ##"postgresql://postgres:aj@localhost/expense"
 
 
-engine = create_engine(
-    DATABASE_URL,
-    poolclass=NullPool,
-)
+engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 
